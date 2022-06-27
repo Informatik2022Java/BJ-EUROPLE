74c555cd1e8e9d267f4b6ca90d8c980d92f9d247
tRoXyy <mitphi19@gmail.com>
2022-06-27 11:59:02 +0200

    added LinkedList details

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,41 @@
 public class LinkedList
 {
-
+ private ListElement first;
+ 
+ public LinkedList()
+ {
+     first=new End();
+ }
+ public LinkedList(Object[] arr)
+ {
+     first=new End();
+     for(int i=0;i<arr.length;i++)
+     {
+         add(arr[i]);
+     }
+ }
+ public void add(Object data)
+ {
+     first=first.add(new DataElement(data));
+ }
+ public Object get(int idx)
+ {
+     return first.get(idx);
+ }
+ public int size()
+ {
+     return first.size();
+ }
+ public void set(int idx,Object data)
+ {
+     first.set(new DataElement(data),idx);
+ }
+ public boolean contains(Object data)
+ {
+     return first.contains(new DataElement(data));
+ }
+ public void print (){
+     first.print();
+ }
+ 
 }
