d363642df9b7a6986cb79d2dd457a02024e093d0
Felix <luttkusfelix@gmail.com>
2022-06-27 11:47:45 +0200

    added vector2

--- Vector2.java
+++ Vector2.java
@@ -1,33 +1,19 @@
-
-/**
- * Beschreiben Sie hier die Klasse Vector2.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
 public class Vector2
-{
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Vector2
-     */
-    public Vector2()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
+{       
+    public float x;
+    public float y;
+       
+    public Vector2() {
+        this.x = 0.0f;
+        this.y = 0.0f;
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+       
+    public Vector2(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+       
+    public boolean equals(Vector2 other) {
+        return (this.x == other.x && this.y == other.y);
     }
 }
