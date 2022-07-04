c50dee8943390dcc6d52cb4a4e3cce05ff0d295d
Larsder <107798574+Larsder@users.noreply.github.com>
2022-07-04 11:38:26 +0200

    Update Countries.java

--- Countries.java
+++ Countries.java
@@ -1,33 +1,59 @@
 
-/**
- * Beschreiben Sie hier die Klasse Countries.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class Countries
+public class Countries extends Graph
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Countries
-     */
+    
     public Countries()
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        super(10);
     }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+    
+    public Countries(Country[] countries, int[][] edges){
+        super(countries, edges);
+    }
+    
+    public static int dist2coords(Vector2 coordsA, Vector2 coordsB){
+        double Lat1 = coordsA.x;
+        double Lon1 = coordsA.y;
+        double Lat2 = coordsB.x;
+        double Lon2 = coordsB.y;
+        //double dist =  Math.acos(Math.cos(Math.toRadians(90-Lat1)) * Math.cos(Math.toRadians(90-Lat2)) + Math.sin(Math.toRadians(90-Lat1)) *  Math.sin(Math.toRadians(90-Lat2)) * Math.cos(Math.toRadians(Lon1-Lon2))) * 3959;
+        //return (int) dist;
+        
+        return (int)(meters(Lat1, Lon1, Lat2, Lon2) / 1000);
+    }
+    
+    private static final double r2d = 180.0D / 3.141592653589793D;
+    private static final double d2r = 3.141592653589793D / 180.0D;
+    private static final double d2km = 111189.57696D * r2d;
+    public static double meters(double lt1, double ln1, double lt2, double ln2) {
+        double x = lt1 * d2r;
+        double y = lt2 * d2r;
+        return Math.acos( Math.sin(x) * Math.sin(y) + Math.cos(x) * Math.cos(y) * Math.cos(d2r * (ln1 - ln2))) * d2km;
+    }
+    
+    public static Country[] getCountriesFromData(String[][] data){
+        Country[] array = new Country[data.length];
+        for(int i = 0; i < data.length; i++){
+            array[i] = new Country(data[i][0].toUpperCase(), data[i][1]);
+        }
+        return array;
+    }
+    
+    public static int[][] getEdgesFromData(String[][] data){
+        int[][] array = new int[data.length][data.length];
+        for(int i = 0; i < data.length; i++){
+            String neightbours[] = data[i][4].split(",");
+            String country = data[i][1];
+            for(int j = 0; j < data.length; j++){
+                for(int k = 0; k < neightbours.length; k++){
+                    if(neightbours[k].equals(data[j][1])){
+                        Vector2 coordsA = new Vector2(Float.parseFloat(data[i][2]), Float.parseFloat(data[i][3]));
+                        Vector2 coordsB = new Vector2(Float.parseFloat(data[j][2]), Float.parseFloat(data[j][3]));                        
+                        array[i][j] = dist2coords(coordsA, coordsB);
+                    }
+                }
+            }
+        }
+        return array;
     }
 }
