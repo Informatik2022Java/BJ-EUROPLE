23cdf16a9d647af01f6c58f51c7b6f9ca15dd8dc
Larsder <107798574+Larsder@users.noreply.github.com>
2022-07-04 11:39:23 +0200

    Update Vertex.java

--- Vertex.java
+++ Vertex.java
@@ -1,33 +1,6 @@
 
-/**
- * Beschreiben Sie hier die Klasse Vertexx.
- * 
- * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
- */
-public class Vertex
+public abstract class Vertex
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
-    /**
-     * Konstruktor für Objekte der Klasse Vertexx
-     */
-    public Vertex()
-    {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
-    }
+    public abstract String getId();
+    public abstract String getName();
 }
